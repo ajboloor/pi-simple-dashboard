64ad2480f00ef2ee323a670eefaece85fcdb5a83
ajboloor <adith94@gmail.com>
2018-03-22 22:01:44 -0500

    commit

--- fetch_data.py
+++ fetch_data.py
@@ -3,8 +3,8 @@ import os
 from subprocess import check_output
 
 def disk_usage(path):
-    "source: https://stackoverflow.com/questions/4260116/find-size-and-free-spa$
-    """Return disk usage statistics about the given path.
+    """source: https://stackoverflow.com/questions/4260116/find-size-and-free-spa
+    Return disk usage statistics about the given path.
 
     Will return the namedtuple with attributes: 'total', 'used' and 'free',
     which are the amount of total, used and free space, in bytes.
@@ -25,13 +25,19 @@ data["device"]["name"] = "potato"
 with open("data_test.json", "w") as jsonFile:
     json.dump(data, jsonFile)
 
-device_name = os.system('uname -n')
-storage_HDD = disk_usage('/mnt/serverhdd/')
-storage_SD = disk_usage('/')
-temp_internal = os.system('echo "scale=1; $(cat /sys/class/thermal/thermal_zone$
-ram_free = float(check_output("cat /proc/meminfo | sed 's: ::g' |  grep -Po '(?$
-ram_used = float(check_output("cat /proc/meminfo | sed 's: ::g' |  grep -Po '(?$
-ram = (round(ram_free/1024,2), round(ram_used/1024,2))
-print storage_HDD
-print storage_SD
-print ram
+def fetch_data():
+    device_name = os.system('uname -n')
+    storage_HDD = disk_usage('/mnt/serverhdd/')
+    storage_SD = disk_usage('/')
+    temp_internal = os.system('echo "scale=1; $(cat /sys/class/thermal/thermal_zone0/temp)/1000" | bc')
+    ram_free = float(check_output("cat /proc/meminfo | sed 's: ::g' |  grep -Po '(?<=MemFree:).*' | grep -Po '.*(?=kB)'",shell=True))
+    ram_used = float(check_output("cat /proc/meminfo | sed 's: ::g' |  grep -Po '(?<=MemAvailable:).*' | grep -Po '.*(?=kB)'",shell=True))
+    ram = (round(ram_free/1024,2), round(ram_used/1024,2))
+
+def main():
+    print storage_HDD
+    print storage_SD
+    print ram
+
+if __name__ == "__main__":
+    main()
